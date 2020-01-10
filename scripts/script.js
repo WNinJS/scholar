637e99c355792d91d07dfb3415edc2ef7bbc8289
beardworf <beardworf@gmail.com>
2020-01-10 14:50:40 +0300

    changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,82 @@
+
+
 am4core.ready(function() {
 
 // Themes begin
 am4core.useTheme(am4themes_animated);
 // Themes end
 
-// Create chart instance
 var chart = am4core.create("chartdiv", am4charts.RadarChart);
+chart.hiddenState.properties.opacity = -1; // this creates initial fade-in
 
-var data = [];
+chart.data = [ 
+	{
+	  "category": "Самоорганизация",
+	  "value": 1,
+	  "color": chart.colors.next()
+	}, {
+	  "category": "Проектирование и моделирование деятельности",
+	  "value": 4,
+	  "color": chart.colors.next()
+	}, 
+	{
+	  "category": "Создание платформ, сетей, экосистем",
+	  "value": 5,
+	  "color": chart.colors.next()
+	}, 
+	{
+	  "category": "Нормативно-правовое регулирование",
+	  "value": 10,
+	  "color": chart.colors.next()
+	}, 
+	{
+	  "category": "Практики управления и орг.деятельности",
+	  "value": 7,
+	  "color": chart.colors.next()
+	}, 
+	{
+	  "category": "Вывод продукта на рынки",
+	  "value": 5,
+	  "color": chart.colors.next()
+	}, 
+	{
+	  "category": "Создание IT-продукта",
+	  "value": 5,
+	  "color": chart.colors.next()
+	}, 
+	{
+	  "category": "Коммуникация",
+	  "value": 10,
+	  "color": chart.colors.next()
+	}, 
+	{
+	  "category": "Работа с будущим",
+	  "value": 7,
+	  "color": chart.colors.next()
+	}
+];
 
-for(var i = 0; i < 24; i++){
-  data.push({category: i, value:Math.round(Math.random() * 100)});
-}
+chart.padding(20, 20, 20, 20);
 
-chart.data = data;
-chart.radius = am4core.percent(100);
-chart.innerRadius = am4core.percent(50);
-
-// Create axes
 var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
 categoryAxis.dataFields.category = "category";
-categoryAxis.renderer.grid.template.location = 0;
-categoryAxis.renderer.minGridDistance = 30;
-categoryAxis.tooltip.disabled = true;
-categoryAxis.renderer.minHeight = 110;
-categoryAxis.renderer.grid.template.disabled = true;
-//categoryAxis.renderer.labels.template.disabled = true;
-let labelTemplate = categoryAxis.renderer.labels.template;
-labelTemplate.radius = am4core.percent(-60);
-labelTemplate.location = 0.5;
-labelTemplate.relativeRotation = 90;
+categoryAxis.renderer.labels.template.location = 0.5;
+categoryAxis.renderer.tooltipLocation = -2;
 
 var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
-valueAxis.renderer.grid.template.disabled = true;
 valueAxis.renderer.labels.template.disabled = true;
-valueAxis.tooltip.disabled = true;
+valueAxis.min = 0;
+valueAxis.max = 20;
+valueAxis.renderer.minGridDistance = 10;
 
-// Create series
 var series = chart.series.push(new am4charts.RadarColumnSeries());
-series.sequencedInterpolation = true;
-series.dataFields.valueY = "value";
-series.dataFields.categoryX = "category";
+series.columns.template.tooltipText = "{categoryX}: {valueY.value}";
+series.columns.template.width = am4core.percent(100);
 series.columns.template.strokeWidth = 0;
-series.tooltipText = "{valueY}";
-series.columns.template.radarColumn.cornerRadius = 10;
-series.columns.template.radarColumn.innerCornerRadius = 0;
+series.columns.template.column.propertyFields.fill = "color";
+series.dataFields.categoryX = "category";
+series.dataFields.valueY = "value";
 
-series.tooltip.pointerOrientation = "vertical";
-
-// on hover, make corner radiuses bigger
-let hoverState = series.columns.template.radarColumn.states.create("hover");
-hoverState.properties.cornerRadius = 0;
-hoverState.properties.fillOpacity = 1;
-
-
-series.columns.template.adapter.add("fill", function(fill, target) {
-  return chart.colors.getIndex(target.dataItem.index);
-})
-
-// Cursor
-chart.cursor = new am4charts.RadarCursor();
-chart.cursor.innerRadius = am4core.percent(50);
-chart.cursor.lineY.disabled = true;
 
 }); // end am4core.ready()
 
@@ -69,9 +84,44 @@ chart.cursor.lineY.disabled = true;
 
 
 
-
-
 // logout button
 function logout() {
-	location.href= "login.html";
+	location.href = "login.html";
+}
+
+
+// load digital print
+function goToLoadPrint() {
+	location.href = "loadprint.html";
+}
+
+
+
+// login functions
+function studentLogin() {
+	location.href = "index.html";
+}
+function teacherLogin() {
+	location.href = "teacherprofile.html";
+}
+
+
+// tabpanel script
+function openTab(e, tabId) {
+	var i, tabcontent, tablink;
+
+	// hide all tabcontent content when tab button is click
+	tabcontent = document.getElementsByClassName('tab-content');
+	for(i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	// remove active for the tablinks if its actvie
+	tablink = document.getElementsByClassName('tab-link');
+	for(i = 0; i < tablink.length; i++) {
+		tablink[i].className = tablink[i].className.replace('btn-left-menu-active', "");
+	}
+
+	document.getElementById(tabId).style.display = "block";
+	e.currentTarget.className += ' btn-left-menu-active';
 }
--- scripts/script_login_signin.js
+++
@@ -1,6 +0,0 @@
-function studentLogin() {
-	location.href = "index.html";
-}
-function teacherLogin() {
-	location.href = "teacherprofile.html";
-}
